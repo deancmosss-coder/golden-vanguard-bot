530dadf5166fbc8e5cb479f6ec17b3f4004006b3
deancmosss-coder <deanc.mosss@gmail.com>
2026-03-21 09:21:33 +0000

    Update refreshWarBoard.js

--- jobs/refreshWarBoard.js
+++ jobs/refreshWarBoard.js
@@ -1,22 +1,11 @@
-const fs = require("fs");
-const path = require("path");
+const { syncWarData } = require("../services/warSync");
 const { updateOperationsBoard } = require("../services/operationsBoard");
-
-const CACHE_FILE = path.join(__dirname, "..", "data", "war_cache.json");
-
-function readJson(file, fallback = {}) {
-  try {
-    return JSON.parse(fs.readFileSync(file, "utf8"));
-  } catch (err) {
-    console.error("[WAR BOARD] Failed reading war_cache.json:", err.message);
-    return fallback;
-  }
-}
 
 async function refreshWarBoard(client) {
   console.log("[WAR BOARD] refreshWarBoard started");
 
-  const warData = readJson(CACHE_FILE, {});
+  const warData = await syncWarData();
+
   console.log("[WAR BOARD] Loaded cache keys:", Object.keys(warData || {}));
 
   await updateOperationsBoard(client, warData);
