f96f8c0796dc02332fb1f41e948a7370f87322e6
deancmosss-coder <deanc.mosss@gmail.com>
2026-05-10 14:30:13 +0100

    Add multiStreamScheduler to ready handler

---
+++ jobs/multiStreamScheduler.js
@@ -0,0 +1,34 @@
+// =========================
+// handlers/readyHandler.js
+// Handles bot ready/startup logic
+// =========================
+
+const { Events } = require("discord.js");
+
+const logger = require("../services/logger");
+const { sendStartupAlert } = require("../services/alertService");
+const { startScheduler } = require("../jobs/scheduler");
+const { startStreamAlertScheduler } = require("../jobs/streamAlertScheduler");
+const { startMultiStreamScheduler } = require("../jobs/multiStreamScheduler");
+
+function registerReadyHandler(client) {
+  client.once(Events.ClientReady, async () => {
+    logger.info(`Logged in as ${client.user.tag}`, {
+      botId: client.user.id,
+    });
+
+    await sendStartupAlert(
+      client,
+      `Golden Vanguard bot is now online as **${client.user.tag}**`
+    );
+
+    await startScheduler(client);
+
+    startStreamAlertScheduler(client);
+    startMultiStreamScheduler(client);
+  });
+}
+
+module.exports = {
+  registerReadyHandler,
+};
