2b33ac77834bb30fbe18f472532bf32b58a0da95
deancmosss-coder <deanc.mosss@gmail.com>
2026-05-15 22:44:34 +0100

    Create config/reviewConfig.js

---
+++ config/reviewConfig.js
@@ -0,0 +1,21 @@
+module.exports = {
+  games: {
+    helldivers: {
+      id: "helldivers",
+      name: "Helldivers 2",
+    },
+  },
+
+  foulWords: [
+    "fuck",
+    "bitch",
+    "cunt",
+    "nigger",
+    "faggot",
+    "retard",
+  ],
+
+  maxReviewLength: 500,
+
+  reviewCooldownHours: 24,
+};
